16a1774207e5c82b8721fa5704eaf25c6a939b7b
Papp László <papp.laszlo.web@gmail.com>
2020-06-09 14:27:52 +0200

    changes

--- includes/config.php
+++ includes/config.php
@@ -9,7 +9,7 @@ if (!session_id())
 
 // Call Facebook API
 
-$facebook = new Facebook\Facebook([
+$facebook = new \Facebook\Facebook([
   'app_id'      => '249506306353410',
   'app_secret'     => 'e7b5cdbf42732f33d084a999a0040795',
   'default_graph_version'  => 'v2.10'
