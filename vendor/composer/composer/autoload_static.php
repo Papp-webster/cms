9ad51b1d29c80033a96d7460ef77597816e93024
Papp László <papp.laszlo.web@gmail.com>
2020-06-09 15:13:34 +0200

    changed better

--- vendor/composer/composer/autoload_static.php
+++ vendor/composer/composer/autoload_static.php
@@ -23,11 +23,6 @@ class ComposerStaticInit35a5a41e77aefdbee88651d76898fd53
     public static $classMap = array (
         'Config' => __DIR__ . '/../..' . '/classes/config.php',
         'Example' => __DIR__ . '/../..' . '/classes/example.php',
-        'PHPMailer\\PHPMailer\\Exception' => __DIR__ . '/..' . '/phpmailer/phpmailer/src/Exception.php',
-        'PHPMailer\\PHPMailer\\OAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/src/OAuth.php',
-        'PHPMailer\\PHPMailer\\PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/src/PHPMailer.php',
-        'PHPMailer\\PHPMailer\\POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/src/POP3.php',
-        'PHPMailer\\PHPMailer\\SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/src/SMTP.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
