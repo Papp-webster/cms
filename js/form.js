af6cbf71178c49098eba35fba345793ba1227035
Papp László <papp.laszlo.web@gmail.com>
2020-10-30 16:18:31 +0100

    form

--- js/form.js
+++ js/form.js
@@ -1,31 +1,4 @@
-// NAVBAR
-
-$(document).ready(function(){
-  var fixHeight = function(){
-    $(".navbar-nav").css("max-height",document.documentElement.clientHeight -150);
-  }
-  fixHeight();
-  $(window).resize(function(){
-   fixHeight();
-  });
-  $(".navbar .navbar-toggler").on("click",function(){
-    fixHeight();
-  });
-  $(".navbar-toggler").on("click",function(){
-    $(".mobileMenu").toggleClass("open");
-  });
-});
-
-
-
-$(".navbar-toggler").click(function() {
-  $(this).toggleClass("active");
-  });
-
-
-
-
-
+// FROM
 
 $("#newUser").click(function(){
   $("#form-main").text("Regisztráció");
