87c2410914132d60ed067cb8439996f61f405d84
Papp László <papp.laszlo.web@gmail.com>
2021-01-05 10:25:15 +0100

    advertise

--- includes/widget.php
+++ includes/widget.php
@@ -1,6 +1,6 @@
 <div class="p-4 mb-3">
     <h4>Ingyenes jogi tanácsadás!</h4>
-    <p>Továbbra is folytatódik ingyenes jogi tanácsadásunk!
+    <p>
         Évkezdő ingyenes jogi tanácsadás napok 2021. január 4-én, hétfőn és január 5-én, kedden 15 órától - 17:30-ig a
         Kölcsey Tér 8. szám alatt. </p>
 
