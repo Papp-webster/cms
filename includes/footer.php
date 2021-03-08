548dc21940b5631c0f53e408842cc7d6034b083d
Papp László <papp.laszlo.web@gmail.com>
2021-03-08 16:17:48 +0100

    footer change

--- includes/footer.php
+++ includes/footer.php
@@ -4,16 +4,15 @@
       <div class="col-md-6">
         <ul>
           <h3>Elérhetőség</h3>
-          <li><i class="fa fa-envelope-open" aria-hidden="true"></i><a href="#">4700, Mátészalka, Kossuth u. 43.</a></li>
+          <li><i class="fa fa-envelope-open" aria-hidden="true"></i><a href="http://maps.google.com/?q=4700 Mátészalka Kossuth u 43" target="_blank">4700, Mátészalka, Kossuth u. 43.</a></li>
           <li>
             <i class="fa fa-envelope"></i>
             <a href="#" id="Btn" data-toggle="modal" data-target="#emailmodal">
-
-              <span>Email</span>
+             <span>Email</span>
             </a>
           </li>
           <li><i class="fab fa-facebook-f" aria-hidden="true"></i><a href="https://www.facebook.com/%C3%89lhet%C5%91-M%C3%A1t%C3%A9szalka-1139052266298049/">Élhető Mátészalka</a></li>
-          <li><i class="fa fa-phone-square" aria-hidden="true"></i><a href="#">+36 20 9405848</a></li>
+          <li><i class="fa fa-phone-square" aria-hidden="true"></i><a href="tel:+36209405848">+36 20 9405848</a></li>
         </ul>
       </div>
     </div>
@@ -33,7 +32,7 @@
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
-          <h4 class="form-main text-center" id="emailmodal">Email</h4>
+          <h4 class="form-main text-center text-white" id="emailmodal">Email</h4>
 
 
 
@@ -42,24 +41,24 @@
             <div id="success"></div>
             <p class="error text-center">* Kötelező mezők!</p>
             <!-- Form -->
-            <form id="contactForm" class="text-center" action="/cms/mail/contact_me.php" name="sentMessage" novalidate="novalidate" autocomplete="off" method="post">
+            <form id="contactForm" class="text-center" action="mail/contact_me.php" name="sentMessage" novalidate="novalidate" autocomplete="off" method="post">
 
               <!-- Name -->
               <div class="form-group">
-                <label for="materialContactFormName">Név</label>
+                <label for="materialContactFormName" class="text-white">Név</label>
                 <input type="text" id="name" class="user form-control" placeholder="*Teljes név" name="name" required="required" data-validation-required-message="Adja meg a nevét!">
                 <p class="help-block text-danger"></p>
               </div>
 
               <!-- E-mail -->
               <div class="form-group">
-                <label for="materialContactFormEmail">E-mail</label>
+                <label for="materialContactFormEmail" class="text-white">E-mail</label>
                 <input type="email" id="email" class="user form-control" placeholder="*Email" name="email" required="required" data-validation-required-message="Adja meg az email címét!">
                 <p class="help-block text-danger"></p>
               </div>
 
               <!-- Subject -->
-              <span>Tárgy:</span>
+              <span class="text-white">Tárgy:</span>
               <select name="obj" id="obj" from="contactForm">
                 <option value="" disabled>Válassz opciót:</option>
                 <option value="Észrevétel" selected>Észrevétel</option>
@@ -97,13 +96,34 @@ $version = 1;
 <!-- jQuery -->
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
 <!-- Bootstrap Core JavaScript -->
-<script src="/cms/js/bootstrap.min.js"></script>
-<script src="/cms/js/pageloader.min.js?data=<?php echo $version; ?>"></script>
-<script src="/cms/js/swiped-events.min.js?data=<?php echo $version; ?>"></script>
-<script src="/cms/js/form.min.js?data=<?php echo $version; ?>"></script>
-<script src="/cms/js/jqBootstrapValidation.min.js?data=<?php echo $version; ?>"></script>
-<script src="/cms/js/contact.min.js?data=<?php echo $version; ?>"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- carousel -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.js"></script>
+<script src="js/carousel.min.js?data=<?php echo $version; ?>"></script>
+<script src="js/pageloader.min.js?data=<?php echo $version; ?>"></script>
+<script src="js/swiped-events.min.js?data=<?php echo $version; ?>"></script>
+<script src="js/form.min.js?data=<?php echo $version; ?>"></script>
+<script src="js/jqBootstrapValidation.min.js?data=<?php echo $version; ?>"></script>
+<script src="js/contact.min.js?data=<?php echo $version; ?>"></script>
+<script src="js/jquery.cookieMessage.min.js?data=<?php echo $version; ?>"></script>
+<script type="text/javascript">
+
+$.cookieMessage({
+    'mainMessage': 'Az Élhető Mátészalka nevű weboldalon Cookie-kat használunk. A korlátlan szolgáltatás garantálásához fogadd el a cookie-k használatát.',
+    'acceptButton': 'Elfogadom',
+    'fontSize': '16px',
+    'backgroundColor': '#666',
+    'btnBackgroundColor':'#3C70E8',
+    'btnFontSize':'11px',
+    'btnFontColor':'white',
+    'expirationDays': 20,
+    'cookieName':'cookieMessage'
+
+});
+</script>
+
 
 </body>
 
